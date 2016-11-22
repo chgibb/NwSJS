22e63d021fd6391a3b2b17942390b2487c45147f
chgibb <chris.gibb@outlook.com>
2016-11-22 12:02:32 -0500

    Fix typo

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[])
         return 1;
     }
     std::vector<std::string> tokens;
-    if(!::MakeWordsFromFile(std::string(argv[1]),tokens,false))
+    if(!::MakeWordsFromFile(std::string(argv[1]),tokens))
     {
         std::cout<<"Could not open "<<argv[1]<<"\n";
         return 1;
