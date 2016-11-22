ff3652c964642579b1af206e6f8907d26aa41c47
chgibb <chris.gibb@outlook.com>
2016-11-22 12:01:06 -0500

    Clean up some formatting and comments

--- MakeWordsFromFile.hpp
+++ MakeWordsFromFile.hpp
@@ -1,11 +1,3 @@
-/*
-	Author: Chris Gibb
-    Student I.D.: 0567901
-    Contact: cgibb@lakeheadu.ca
-    Instructor: Dr. Sabah Mohammed
-    Course: CS 2430
-    Modified From Original By Chris Gibb to Include C-Style Comment Stripping
-*/
 #pragma once
 #include <string>
 #include <vector>
@@ -13,7 +5,7 @@
 #include <algorithm>
 //parse file identified by string filename into individual words
 //held in tokenlist
-bool MakeWordsFromFile(std::string filename,std::vector<std::string>&tokenlist,bool lowercase = true)
+bool MakeWordsFromFile(std::string filename,std::vector<std::string>&tokenlist)
 {
     std::ifstream file(filename.c_str(),std::ios::in);
     char byte;
@@ -64,89 +56,61 @@ bool MakeWordsFromFile(std::string filename,std::vector<std::string>&tokenlist,b
                 }
             break;
             case ' ':
-                //cull common punctuation and formating characters
                 str.erase(std::remove(str.begin(),str.end(),'\n'),str.end());
                 str.erase(std::remove(str.begin(),str.end(),'\t'),str.end());
-                //make everything lowercase
-                if(lowercase)
-                    std::transform(str.begin(),str.end(),str.begin(),::tolower);
                 if(str != "")
                     tokenlist.push_back(str);
                 str = "";
                 add = false;
             break;
             case '(':
-                //cull common punctuation and formating characters
                 str += "(";
                 str.erase(std::remove(str.begin(),str.end(),'\n'),str.end());
                 str.erase(std::remove(str.begin(),str.end(),'\t'),str.end());
-                //make everything lowercase
-                if(lowercase)
-                    std::transform(str.begin(),str.end(),str.begin(),::tolower);
                 if(str != "")
                     tokenlist.push_back(str);
                 str = "";
                 add = false;
             break;
             case ')':
-                //cull common punctuation and formating characters
                 str += ")";
                 str.erase(std::remove(str.begin(),str.end(),'\n'),str.end());
                 str.erase(std::remove(str.begin(),str.end(),'\t'),str.end());
-                //make everything lowercase
-                if(lowercase)
-                    std::transform(str.begin(),str.end(),str.begin(),::tolower);
                 if(str != "")
                     tokenlist.push_back(str);
                 str = "";
                 add = false;
             break;
             case '{':
-                //cull common punctuation and formating characters
                 str += "{";
                 str.erase(std::remove(str.begin(),str.end(),'\n'),str.end());
                 str.erase(std::remove(str.begin(),str.end(),'\t'),str.end());
-                //make everything lowercase
-                if(lowercase)
-                    std::transform(str.begin(),str.end(),str.begin(),::tolower);
                 if(str != "")
                     tokenlist.push_back(str);
                 str = "";
                 add = false;
             break;
             case '}':
-                //cull common punctuation and formating characters
                 str += "}";
                 str.erase(std::remove(str.begin(),str.end(),'\n'),str.end());
                 str.erase(std::remove(str.begin(),str.end(),'\t'),str.end());
-                //make everything lowercase
-                if(lowercase)
-                    std::transform(str.begin(),str.end(),str.begin(),::tolower);
                 if(str != "")
                     tokenlist.push_back(str);
                 str = "";
                 add = false;
             break;
             case ',':
-                //cull common punctuation and formating characters
                 str += ",";
                 str.erase(std::remove(str.begin(),str.end(),'\n'),str.end());
                 str.erase(std::remove(str.begin(),str.end(),'\t'),str.end());
-                //make everything lowercase
-                if(lowercase)
-                    std::transform(str.begin(),str.end(),str.begin(),::tolower);
                 if(str != "")
                     tokenlist.push_back(str);
                 str = "";
                 add = false;
             break;
             case '\n':
-                //cull common punctuation and formating characters
                 str.erase(std::remove(str.begin(),str.end(),'\n'),str.end());
                 str.erase(std::remove(str.begin(),str.end(),'\t'),str.end());
-                //make everything lowercase
-                if(lowercase)
-                    std::transform(str.begin(),str.end(),str.begin(),::tolower);
                 if(str != "")
                 tokenlist.push_back(str);
                 str = "";
