7771aae498e4b0b00eeffe749ae5e7dd51646cfa
Chris Gibb <chris.gibb@outlook.com>
2017-03-21 15:59:12 -0400

    Try to detect travis and change g++ programmatically

--- build.bash
+++ build.bash
@@ -1,7 +1,14 @@
 rm nwsjs
-g++ --version
+
+CXX = "g++"
+if [ "$travis" = true ]; then 
+    CXX = "g++-5.4"
+    printf "Detected TravisCI\n"
+fi
+
+CXX --version
 printf "Building nwsjs\n"
 
-g++ -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11  -c main.cpp -o main.o
-g++  -o nwsjs main.o  -s  
+CXX -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11  -c main.cpp -o main.o
+CXX  -o nwsjs main.o  -s  
 rm *.o
