8ef21d3a57b6207580b1c4b840d60c5bff5291aa
Chris Gibb <chris.gibb@outlook.com>
2017-03-21 16:05:08 -0400

    Fix statements

--- build.bash
+++ build.bash
@@ -1,14 +1,14 @@
 rm nwsjs
 
-CXX = "g++"
+CXX="g++"
 if [ "$travis" = true ]; then 
-    CXX = "g++-5.4"
+    CXX="g++-5.4"
     printf "Detected TravisCI\n"
 fi
 
-CXX --version
+$CXX --version
 printf "Building nwsjs\n"
 
-CXX -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11  -c main.cpp -o main.o
-CXX  -o nwsjs main.o  -s  
+$CXX -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11  -c main.cpp -o main.o
+$CXX  -o nwsjs main.o  -s  
 rm *.o
