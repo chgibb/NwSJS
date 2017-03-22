e86e0af53388ebfebc6badad53b12b21fd4c0d67
Chris Gibb <chris.gibb@outlook.com>
2017-03-22 11:17:56 -0400

    Show file sizes and stop showing times

--- test.bash
+++ test.bash
@@ -1,8 +1,10 @@
 for f in node_modules/typescript/lib/*.js
 do
-    printf "Compressing $f\n"
-    time ./nwsjs $f --comments --spaces --tabs > tmp
+
+    printf "$f original size: $(wc -c < $f)\n"
+    ./nwsjs $f --comments --spaces --tabs > tmp
     mv tmp $f
+    printf "$f compressed size $(wc -c < $f)\n\n\n\n"
 done
 
 for f in *.ts
@@ -18,10 +20,12 @@ do
         exit 1
     fi
     printf "Compressing Browserify bundle\n"
-    time ./nwsjs bundle$artifact --comments --spaces --tabs > compBundle$artifact
+    printf "bundle$artifact original size: $(wc -c < bundle$artifact)\n"
+    ./nwsjs bundle$artifact --comments --spaces --tabs > compBundle$artifact
     if [ $? != 0 ]; then
         exit 1
     fi
+    printf "bundle$artifact compressed size $(wc -c < compBundle$artifact)\n\n\n\n"
 
     node bundle$artifact
     retValBundle=$?
