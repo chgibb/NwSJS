45d65d3ef913f4c377da020f51a91bf5dd28f9f1
chgibb <chris.gibb@outlook.com>
2017-03-23 10:27:57 -0400

    Added static settings to linker

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 set path=%path%;C:\mingw\bin
 g++ -c -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11 -static -g main.cpp
-g++ -o nwsjs main.o  -s
+g++ -static -static-libgcc -static-libstdc++ -o nwsjs main.o  -s
