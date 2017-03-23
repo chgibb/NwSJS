c736c35e11e0ea439053715edc2433ed967dc538
chgibb <chris.gibb@outlook.com>
2017-03-23 14:00:34 -0400

    Refreshed git cache

--- build.bat
+++
@@ -1,3 +0,0 @@
-set path=%path%;C:\mingw\bin
-g++ -c -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11 -static -g main.cpp
-g++ -static -static-libgcc -static-libstdc++ -o nwsjs main.o  -s
--- refreshTests.bash
+++
@@ -1,2 +0,0 @@
-rm -rf test262
-git clone https://github.com/tc39/test262
