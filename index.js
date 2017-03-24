3ba76accf9e61dc16c3bb22b0541f79b20da9e4c
Chris Gibb <chris.gibb@outlook.com>
2017-03-24 19:01:46 -0400

    Proto npm package files

---
+++ index.js
@@ -0,0 +1,12 @@
+var spawn = require("child_process");
+var args = process.argv;
+args.splice();
+args.splice();
+if(process.platform == "linux")
+{
+    spawn.spawnSync("nwsjslinux",args);
+}
+if(process.platform == "win32")
+{
+    spawn.spawnSync("nwsjswin",args);
+}
