be19e560165693d342cd216f351bd6f39c3967a4
Chris Gibb <chris.gibb@outlook.com>
2017-03-24 18:18:47 -0400

    Changed paths to point to test folder

--- test.bash
+++ test.bash
@@ -30,7 +30,7 @@ do
 done
 
 #for each of our .ts test files
-for f in *.ts
+for f in tests/*.ts
 do
     #try to run our now compressed Typescript compiler over them
     ./node_modules/.bin/tsc $f
@@ -45,31 +45,33 @@ do
     #Executable bit on Browserify is getting clobbered by compression for some reason. Set it explicitly
     chmod +x node_modules/.bin/browserify
 
+    bundle="bundle.js"
+    compBundle="compBundle.js"
     #use browserify to bundle the file and save it to bundleOutput.js
-    ./node_modules/.bin/browserify $artifact --node --debug -o bundle$artifact
+    ./node_modules/.bin/browserify $artifact --node --debug -o $artifact$bundle
     if [ $? != 0 ]; then
         exit 1
     fi
 
     #Print the file size of the bundle and then compress it
     printf "Compressing Browserify bundle\n"
-    printf "bundle$artifact original size: $(wc -c < bundle$artifact)\n"
-    ./nwsjs bundle$artifact --comments --spaces --tabs --newLines > compBundle$artifact
+    printf "bundle$artifact original size: $(wc -c < $artifact$bundle)\n"
+    ./nwsjs $artifact$bundle --comments --spaces --tabs --newLines > $artifact$compBundle
     #If we've failed to compress the bundle then it's likely to too complex and nwsjs has tripped on itself trying to compress it
     if [ $? != 0 ]; then
         exit 1
     fi
 
     #print the file size of the compressed bundle
-    printf "bundle$artifact compressed size $(wc -c < compBundle$artifact)\n\n\n\n"
+    printf "bundle$artifact compressed size $(wc -c < $artifact$compBundle)\n\n\n\n"
 
     #Run the uncompressed bundle
-    node bundle$artifact &> "bundle$artifact".log
+    node $artifact$bundle &> "$artifact$bundle".log
     #Save it's return value
     retValBundle=$?
 
     #Run the compressed bundle
-    node compBundle$artifact &> "compBundle$artifact".log
+    node $artifact$compBundle &> "$artifact$compBundle".log
     #If the return values differ from running the uncompressed and compressed bundles, then we've probably broken something
     #We test return values between the two because some tests use npm packages that don't work in Node and cause it to crash
     #These are used because they're very large and are good to stress test our compressor.
