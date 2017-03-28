ea1cbb3049db1c009ad849cb2289a5173bc6651c
Chris Gibb <chris.gibb@outlook.com>
2017-03-28 14:43:09 -0400

    Created function

---
+++ inc/ignoreWrappedSequence.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include "StreamBuffer.hpp"
+namespace nwsjs
+{
+    std::string ignoreWrappedSequence(
+        nwsjs::StreamPassBuffer&buff,
+        unsigned int&i,
+        size_t end,
+        char ignore
+    )
+    {
+        if(buff.bytes[i].byte == ignore)
+        {
+            std::string str = "";
+            while(i != end)
+            {
+                str += buff.bytes[i].byte;
+                if(buff.bytes[i].byte == ignore && buff.bytes[i].byte != '\\')
+                    return str;
+                i++;
+            }
+        }
+        return "";
+    }
+}
