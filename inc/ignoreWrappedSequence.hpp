8ff3ebe1a823cbae429d7c0c163cc4e3a663f286
Chris Gibb <chris.gibb@outlook.com>
2017-03-29 17:16:31 -0400

    Fixed bug

--- inc/ignoreWrappedSequence.hpp
+++ inc/ignoreWrappedSequence.hpp
@@ -13,15 +13,16 @@ namespace nwsjs
         if(buff.bytes[i].byte == ignore)
         {
             std::string str = "";
+            str += ignore;
             while(i != end)
             {
+                i++;
                 str += buff.bytes[i].byte;
-                if(buff.bytes[i].byte == ignore && buff.bytes[i].byte != '\\')
+                if(buff.bytes[i].byte == ignore && buff.bytes[i-1].byte != '\\')
                 {
                     i++;
                     return str;
                 }
-                i++;
             }
         }
         return "";
