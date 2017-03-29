7b790f1937a8ce19b46513cdc5a40a4dd50da22f
Chris Gibb <chris.gibb@outlook.com>
2017-03-29 17:39:46 -0400

    Cleaned old code

--- main.cpp
+++ main.cpp
@@ -36,24 +36,5 @@ int main(int argc, char* argv[])
         std::cerr<<"Could not open "<<argv[1]<<std::endl;
     }
     buff.print(std::cout);
-    /*if(!nwsjs::options::newLines)
-    {
-        if(!nwsjs::tokenizeAndCompress<decltype(std::cout)>(std::string(argv[1]),std::cout))
-        {
-            std::cout<<"Could not open "<<argv[1]<<"\n";
-            return 1;
-        }
-    }
-    else if(nwsjs::options::newLines)
-    {
-        nwsjs::StreamPassBuffer passBuff;
-        nwsjs::tokenizeAndCompress<decltype(passBuff)>(std::string(argv[1]),passBuff);
-        nwsjs::secondPassCompression(passBuff);
-        for(unsigned int i = 0; i != passBuff.bytes.size(); ++i)
-        {
-            if(passBuff.bytes[i].stream)
-                std::cout<<passBuff.bytes[i].byte;
-        }
-    }*/
     return 0;
 }
